fc4268bf286aff63d20c4910eeb747e21679cb90
Ramon Pereira <ramon.pereira@luizalabs.com>
2021-10-10 18:52:43 -0300

    initial commit

---
+++ src/main/java/ramon/pereira/password/validate/Application.java
@@ -0,0 +1,13 @@
+package ramon.pereira.password.validate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
