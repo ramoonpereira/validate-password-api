6eaf24f6127c0caf64e89d1e44d2d9f4f73d6fb2
Ramon Pereira <apereira.ramon@gmail.com>
2021-10-13 23:21:42 -0300

    testing WithPasswordValidateValidatorImplTest rules

--- src/test/java/ramon/pereira/password/validate/validator/WithPasswordValidateValidatorImplTest.java
+++ src/test/java/ramon/pereira/password/validate/validator/WithPasswordValidateValidatorImplTest.java
@@ -36,4 +36,144 @@ public class WithPasswordValidateValidatorImplTest {
                 .isNotNull()
                 .isExactlyInstanceOf(WithPasswordValidateValidatorImpl.class);
     }
+
+    @Test
+    void executePasswordValidateValidatorValidRepeatedCharactersContainsRepeteadExpectedPasswordInvalidException() {
+        WithPasswordValidateValidator passwordValidateValidator =
+                new WithPasswordValidateValidatorImpl("1234456789");
+
+        assertThatThrownBy(passwordValidateValidator::validRepeatedCharacters)
+                .isExactlyInstanceOf(PasswordInvalidException.class);
+    }
+
+    @Test
+    void executePasswordValidateValidatorValidRepeatedCharactersStringIsValidExpectedSuccess() {
+        WithPasswordValidateValidator passwordValidateValidator =
+                new WithPasswordValidateValidatorImpl("123456789");
+
+        final var passwordValidate = passwordValidateValidator.validRepeatedCharacters();
+
+        assertThat(passwordValidate)
+                .isNotNull()
+                .isExactlyInstanceOf(WithPasswordValidateValidatorImpl.class);
+    }
+
+    @Test
+    void executePasswordValidateValidatorValidSpecialCharactersNotContainsSpecialExpectedPasswordInvalidException() {
+        WithPasswordValidateValidator passwordValidateValidator =
+                new WithPasswordValidateValidatorImpl("123456789");
+
+        assertThatThrownBy(passwordValidateValidator::validSpecialCharacters)
+                .isExactlyInstanceOf(PasswordInvalidException.class);
+    }
+
+    @Test
+    void executePasswordValidateValidatorValidSpecialCharactersContainsSpecialExpectedSuccess() {
+        WithPasswordValidateValidator passwordValidateValidator =
+                new WithPasswordValidateValidatorImpl("123456789&");
+
+        final var passwordValidate = passwordValidateValidator.validSpecialCharacters();
+
+        assertThat(passwordValidate)
+                .isNotNull()
+                .isExactlyInstanceOf(WithPasswordValidateValidatorImpl.class);
+    }
+
+    @Test
+    void executePasswordValidateValidatorValidWhitespaceContainsWhitespaceExpectedPasswordInvalidException() {
+        WithPasswordValidateValidator passwordValidateValidator =
+                new WithPasswordValidateValidatorImpl("123456789 &");
+
+        assertThatThrownBy(passwordValidateValidator::validWhitespace)
+                .isExactlyInstanceOf(PasswordInvalidException.class);
+
+
+    }
+
+    @Test
+    void executePasswordValidateValidatorValidWhitespaceNotContainsWhitespaceExpectedSuccess() {
+        WithPasswordValidateValidator passwordValidateValidator =
+                new WithPasswordValidateValidatorImpl("123456789&");
+
+        final var passwordValidate = passwordValidateValidator.validWhitespace();
+
+        assertThat(passwordValidate)
+                .isNotNull()
+                .isExactlyInstanceOf(WithPasswordValidateValidatorImpl.class);
+
+    }
+
+    @Test
+    void executePasswordValidateValidatorValidDigitsNotContainsDigitsExpectedPasswordInvalidException() {
+        WithPasswordValidateValidator passwordValidateValidator =
+                new WithPasswordValidateValidatorImpl("ABCDEFGHIJ");
+
+        assertThatThrownBy(passwordValidateValidator::validDigits)
+                .isExactlyInstanceOf(PasswordInvalidException.class);
+
+
+    }
+
+    @Test
+    void executePasswordValidateValidatorValidDigitsContainsDigitsExpectedSuccess() {
+        WithPasswordValidateValidator passwordValidateValidator =
+                new WithPasswordValidateValidatorImpl("123456789&");
+
+        final var passwordValidate = passwordValidateValidator.validDigits();
+
+        assertThat(passwordValidate)
+                .isNotNull()
+                .isExactlyInstanceOf(WithPasswordValidateValidatorImpl.class);
+
+    }
+
+    @Test
+    void executePasswordValidateValidatorValidCharacterLowerCaseNotContainsLowerCaseExpectedPasswordInvalidException() {
+        WithPasswordValidateValidator passwordValidateValidator =
+                new WithPasswordValidateValidatorImpl("ABCDEFGHIJ");
+
+        assertThatThrownBy(passwordValidateValidator::validCharacterLowerCase)
+                .isExactlyInstanceOf(PasswordInvalidException.class);
+
+
+    }
+
+    @Test
+    void executePasswordValidateValidatorValidCharacterLowerCaseContainsLowerCaseExpectedSuccess() {
+        WithPasswordValidateValidator passwordValidateValidator =
+                new WithPasswordValidateValidatorImpl("abcDEFGHIJ&");
+
+        final var passwordValidate = passwordValidateValidator.validCharacterLowerCase();
+
+        assertThat(passwordValidate)
+                .isNotNull()
+                .isExactlyInstanceOf(WithPasswordValidateValidatorImpl.class);
+
+    }
+
+    @Test
+    void executePasswordValidateValidatorValidCharacterUpperCaseNotContainsUpperCaseExpectedPasswordInvalidException() {
+        WithPasswordValidateValidator passwordValidateValidator =
+                new WithPasswordValidateValidatorImpl("abcdefghij");
+
+        assertThatThrownBy(passwordValidateValidator::validCharacterUpperCase)
+                .isExactlyInstanceOf(PasswordInvalidException.class);
+
+
+    }
+
+    @Test
+    void executePasswordValidateValidatorValidCharacterUpperCaseContainsUpperCaseExpectedSuccess() {
+        WithPasswordValidateValidator passwordValidateValidator =
+                new WithPasswordValidateValidatorImpl("abcDEFGHIJ&");
+
+        final var passwordValidate = passwordValidateValidator.validCharacterUpperCase();
+
+        assertThat(passwordValidate)
+                .isNotNull()
+                .isExactlyInstanceOf(WithPasswordValidateValidatorImpl.class);
+
+    }
+
+
 }
