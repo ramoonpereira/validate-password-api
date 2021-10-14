cfa58bcded8915abf886cc56a05d7e3bf31e58eb
Ramon Pereira <apereira.ramon@gmail.com>
2021-10-13 23:45:31 -0300

    implements tests in password mapper

---
+++ src/test/java/ramon/pereira/password/validate/controller/v1/PasswordValidateControllerTest.java
@@ -0,0 +1,4 @@
+package ramon.pereira.password.validate.controller.v1;
+
+public class PasswordValidateControllerTest {
+}
---
+++ src/test/java/ramon/pereira/password/validate/controller/v1/PasswordValidateMapperTest.java
@@ -0,0 +1,32 @@
+package ramon.pereira.password.validate.controller.v1;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ramon.pereira.password.validate.controller.v1.dtos.PasswordValidateRequestDTO;
+import ramon.pereira.password.validate.controller.v1.mapper.PasswordValidateMapper;
+import ramon.pereira.password.validate.model.PasswordValidateModel;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@ExtendWith(MockitoExtension.class)
+public class PasswordValidateMapperTest {
+
+    @Test
+    void executePasswordValidateMapperToModelIsNullExpectNullPointerException() {
+        assertThatThrownBy(() -> PasswordValidateMapper.toModel(null))
+                .isExactlyInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void executePasswordValidateMapperToModelIsValidExpectSuccess() {
+        final var passwordDTO = PasswordValidateRequestDTO.builder()
+                .password("123456789")
+                .build();
+
+        assertThat(PasswordValidateMapper.toModel(passwordDTO))
+                .isNotNull()
+                .isExactlyInstanceOf(PasswordValidateModel.class);
+    }
+}
